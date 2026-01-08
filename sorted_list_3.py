0e75b606675bcda36e8e34f559448fcf1ec6aa60
catgame-coder <alenasolnishko555@gmail.com>
2026-01-08 12:04:54 +0500

    add the min and max year for range selection

--- sorted_list_3.py
+++ sorted_list_3.py
@@ -13,3 +13,20 @@ def get_sorted_list_3(books, n1, n2):
     books_copy = [b.copy() for b in filtered]
     heap_sort_custom(books_copy, compare_report3)
     return books_copy
+
+def get_year_range(books):
+    '''Возвращает кортеж (min_year, max_year) из списка книг.'''
+    if not books:
+        raise ValueError("Список книг пуст. Проверьте файл books.txt.")
+
+    min_year = books[0]['year']
+    max_year = books[0]['year']
+
+    for book in books[1:]:
+        year = book['year']
+        if year < min_year:
+            min_year = year
+        if year > max_year:
+            max_year = year
+
+    return (min_year, max_year)
